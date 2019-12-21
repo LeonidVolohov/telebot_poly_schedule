e3f2e99dd4f236a321f298d464745b08920f0ce5
LeonidVolohov <leka.volohov@gmail.com>
2019-12-21 16:59:24 +0300

    Add tests for file "group_funcs.py"

---
+++ tests/test_group_funcs.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+sys.path.insert(1, '/home/leonid/Documents/github/bot_evgenij/telebot_poly_schedule/code/utils/')
+
+import group_funcs
+
+
+class TestGroupFuncs(unittest.TestCase):
+
+    def test_parse_group_from_user(self):
+
+        with self.assertRaises(SyntaxError):
+            group_funcs.parse_group_from_user('')
+
+        with self.assertRaises(SyntaxError):
+            group_funcs.parse_group_from_user('/set_group')
+            print()
+
+        with self.assertRaises(SyntaxError):
+            group_funcs.parse_group_from_user('/set_group 3530904/70101 abc')
+
+        with self.assertRaises(ValueError):
+            group_funcs.parse_group_from_user('/set_group 3530904/70101')
+
+    def test_remember_relation(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
