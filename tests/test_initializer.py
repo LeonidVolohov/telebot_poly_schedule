a3d89e5c38c5e3e4d528e8c217fce471a5081d41
LeonidVolohov <leka.volohov@gmail.com>
2019-12-21 17:00:23 +0300

    Add test for file "initializer.py"

---
+++ tests/test_initializer.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+sys.path.insert(1, '/home/leonid/Documents/github/bot_evgenij/telebot_poly_schedule/code/web_tools/')
+
+import initializer
+
+
+class TestInitializer(unittest.TestCase):
+    def test_get_groups(self):
+        
+        self.assertEqual(initializer.get_groups('http://ruz.spbstu.ru/api/v1/ruz')['3844502/80101'], (101, 28116))
+
+        self.assertEqual(initializer.get_groups('http://ruz.spbstu.ru/api/v1/ruz')['з3532705/80001'], (95, 29141))
+
+        self.assertNotEqual(initializer.get_groups('http://ruz.spbstu.ru/api/v1/ruz')['з3532705/80001'], 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
